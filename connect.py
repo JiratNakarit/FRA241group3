8fe51c3ec1c6a26d0fe71510a608e523b6bcfa62
rabayjai112 <rabayjai112@gmail.com>
2016-10-22 14:23:45 +0700

    1st connection class

---
+++ connect.py
@@ -0,0 +1,61 @@
+import MySQLdb
+
+class Database():
+    def __init__(self):
+        self.database = MySQLdb.connect(host = "localhost", user = "root",passwd = "", db = "test")
+
+    def cursor(self):
+        return self.database.cursor()
+
+    def IncludeColumn(self,data):
+        column = []
+        count = 1
+        pointer = 0
+
+        for i in data:
+            if count == 1:
+                for k in i:
+                    column.append([])
+
+            for j in i:
+                column[pointer].append(j)
+                pointer += 1
+            pointer = 0
+            count += 1
+
+        return column
+
+
+class Connect_data(Database):
+    db = Database()
+    cursor = db.cursor()
+    sql = "SELECT * from EMPLOYEE"
+
+    cursor.execute(sql)
+
+    data = cursor.fetchall()
+
+    print db.IncludeColumn(data)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""db = connect()
+cursor = db.cursor()
+sql = "SELECT * from EMPLOYEE"
+
+cursor.execute(sql)
+
+data = cursor.fetchall()
+
+print data"""
