3a15ed0a2866c6a83d38da584d4cfc148dd8e586
rabayjai112 <rabayjai112@gmail.com>
2016-10-22 22:46:50 +0700

    2st connection class

--- connect.py
+++ connect.py
@@ -1,16 +1,15 @@
 import MySQLdb
 
-
 class Database():
     def __init__(self):
         self.database = MySQLdb.connect(host="localhost", user="root", passwd="", db="fra241")
         self.cursor = self.database.cursor()
 
-    def getData(self,table, column):
+    def getData(self, table, column):
         sql = "SELECT " + str(column) + " FROM " + str(table)
         return sql
 
-    def includeColumn(self,data):
+    def includeColumn(self, data):
         column = []
         count = 1
         pointer = 0
@@ -28,10 +27,8 @@ class Database():
 
         return column
 
-
-
-    def data_user(self,cursor, column):
-        sql = self.getData("user","*")
+    def data_user(self, cursor, column):
+        sql = self.getData("user", "*")
         cursor.execute(sql)
         data = cursor.fetchall()
         list_column = self.includeColumn(data)
@@ -45,7 +42,7 @@ class Database():
         data = cursor.fetchall()
         list_column = self.includeColumn(data)
         if column != "*":
-            return list_column[column-1]
+            return list_column[column - 1]
         else:
             return list_column
 
@@ -54,7 +51,7 @@ class Database():
         data = cursor.fetchall()
         list_column = self.includeColumn(data)
         if column != "*":
-            return list_column[column-1]
+            return list_column[column - 1]
         else:
             return list_column
 
@@ -63,11 +60,10 @@ class Database():
         data = cursor.fetchall()
         list_column = self.includeColumn(data)
         if column != "*":
-            return list_column[column-1]
+            return list_column[column - 1]
         else:
             return list_column
 
-    #name = data_user(db,cursor,2)
-    #stid = data_user(db,cursor,4)
-    #num_in_machine = data_machine(db,cursor,2)
-
+            # name = data_user(db,cursor,2)
+            # stid = data_user(db,cursor,4)
+            # num_in_machine = data_machine(db,cursor,2)
