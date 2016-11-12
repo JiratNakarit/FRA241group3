2a4236b25ae5625df325e13a3f8bd1f099a0e48a
win <winzwonwon@hotmail.com>
2016-11-12 19:55:20 +0700

    update

--- Adminstator.py
+++ Adminstator.py
@@ -1,9 +1,13 @@
 from Class import *
-list1=[['74hc64','Drive_motor'],['74hc00','T_Type']]
-D = UpDown_num()
-for i in list1:
+from Admin import *
+A = Admin()
+list = A.conferir()
+print list
+#D = UpDown_num()
+'''for i in list:
      value_ic = i[0]
      value_type = i[1]
-     D.Admin(value_ic,value_type)
-     D.Commit()
+     print value_ic,value_type
+     #D.Admin(value_ic,value_type)
+     #D.Commit()'''
 
--- Class.py
+++ Class.py
@@ -6,12 +6,6 @@ class UpDown_num:
         self.db = MySQLdb.connect(host="localhost", user="root", passwd="fra241", db="test")
         self.cursor = self.db.cursor()
 
-    '''def D_Typeupdate(self,iv):
-        self.sql = "UPDATE trello SET Available = Available +" + " " + str(iv) + " " + "WHERE IC = '%s'" % ('D_Type')
-
-    def T_Typeupdate(self,iv):
-        self.sql = "UPDATE trello SET Available = Available +" + " " + str(iv) + " " + "WHERE IC = '%s'" % ('T_Type')'''
-
     def Update(self,icid,numall):
         self.sql = "UPDATE machine SET numall = numall -" + " " + str(numall) + " " + "WHERE icid = '%s'" % (str(icid))         #Arm
 
@@ -19,11 +13,12 @@ class UpDown_num:
         self.sql = "UPDATE trello SET Available = Available -" + " " + str(numall) + " " + "WHERE IC = '%s'" % (str(icid))
 
 
-    def Admin(self,value_ic,value_type):
-        self.sql = "INSERT INTO `ic`(`icid`, `type`) VALUES" + " " + "('%s','%s')" %(str(value_ic),str(value_type))             #Bank
+    def ic(self,value_type,value_name):
+        self.sql = "INSERT INTO `ic`(`type`,`name`) VALUES" + " " + "('%s','%s')" %(str(value_type),str(value_name))             #Bank
+        #self.sql = "INSERT INTO `machine`(`numall`) VALUES" + " " + "('%d')" % (value_numall)
 
-    def trello(self):
-        self.sql = "INSERT INTO `trello`(`IC`) VALUES" + " " + "('%s')" %(str('eieiei'))
+    def machine(self,value_numall):
+        self.sql = "INSERT INTO `machine`(`numall`) VALUES" + " " + "('%d')" %(value_numall)
 
     def Commit(self):
 
